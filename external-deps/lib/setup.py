f6fd95412be4ea60d828b896eee022dd75981d5d
novaya <shkolanovaya@gmail.com>
2022-05-11 21:11:52 -0500

    Added example files and pip_list

---
+++ external-deps/lib/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup, Extension
+
+setup(name="sample",
+      ext_modules=[
+        Extension("sample",
+                  ["sample.c", "pysample.c"],
+                  include_dirs=['.'],
+                  )
+        ]
+)
